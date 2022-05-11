ca81bf56d2aae834f64c9a12ab6e55785c713ad2
Maryna Kuzminchuk <Maryna.Kuzminchuk@Student.HTW-Berlin.de>
2022-05-11 21:34:37 +0200

    Integrate with MongoDB

--- main.js
+++ main.js
@@ -13,11 +13,12 @@ const port = 3000;
 // app.listen(3000);
 
 // connect to mongodb database
-// const dbURI = 'mongodb+srv://<username>:<password>@foodorder.enn28.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const dbURI = 'mongodb+srv://<username>:<password>@foodorder.enn28.mongodb.net/FoodOrder?retryWrites=true&w=majority'
 // maryna = p9Xn5MYjNaeklQMU
 // joey = j5dfn2MQnuHjZGp5
 // lana = UOwUBbYgUXhY1JQF
 // elsya = tgTEGOOfLC8PLzEq
+// anni = i4eIB3sN6sAJ7pZP
 
 const dbURI = 'mongodb+srv://anni:i4eIB3sN6sAJ7pZP@foodorder.enn28.mongodb.net/FoodOrder?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -40,16 +41,6 @@ app.get("/", (req, res) => {
 app.get("/menu/items/:itemId", menuController.getItem);
 
 app.get("/menu", menuController.getMenu);
-
-// Not needed anymore as well as veggie.ejs
-// Replaced with vegetarian=true query parameter for /menu route in menuController
-app.get("/veggie", (req, res) => {
-  const veggie = [
-     { "id": 4, "title": "Avocado Nigiri", "image": "https://imageproxy.wolt.com/menu/menu-images/6019324568bc6b99044013c5/afd770b8-6566-11eb-a08c-0a89d2884f48_avocadonigiri.jpeg", "price": 5.00, "weight": 30, "fats": 1.33, "carbohydrates": 35.10, "calories": 120.00},
-     { "id": 5, "title": "Avocado Nigiri", "image": "https://imageproxy.wolt.com/menu/menu-images/6019324568bc6b99044013c5/99c84496-6566-11eb-9bfa-fe9c1eb06953_inari.jpeg", "price": 4.50, "weight": 56, "fats": 2.33, "carbohydrates": 41.10, "calories": 134.00 },
-  ];
-    res.render("veggie", {title: "Vegetarian", veggie });
-});
 
 app.get("/about", (req, res) => {
     //res.sendFile(__dirname + "/views/menu.html");
