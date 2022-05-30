f84e55f1ae89ee3b5cc245e4a42aa10045d2c687
Lana_Alzbibi <LanaAlzbibi@gmail.com>
2022-05-30 23:00:57 +0200

    small CSS and Mongoose changes

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,211 +1,190 @@
-'use scrict'
+"use scrict";
 // https://stackoverflow.com/questions/59174763/how-to-add-product-to-shopping-cart-with-nodejs-express-and-mongoose
 
 // Just created file, couldnt finish it
-const Cart = require('./../models/cart');
+const Cart = require("./../models/cart");
 
 // 1. Check if the cart already exists in the database
 // 2. If the cart doesn't exist, create a new cart with the given userID, item, total cost and save it in the database
 // 3. If the cart exists, add item to the cart, update total cost and save it in the database
 
-module.exports = { 
+module.exports = {
+  addItem: (req, res) => {
+    Cart.findOne({ userID: req.body.userID })
+      .exec()
+      .then((cart) => {
+        if (cart == null) {
+          cart = new Cart({
+            userID: req.body.userID,
+            items: [
+              {
+                title: req.body.title,
+                price: parseFloat(req.body.price),
+                quantity: 1,
+              },
+            ],
+            totalCost: req.body.price,
+          });
+          cart
+            .save()
+            .then((result) => {
+              res.send("Item added to cart");
+            })
+            .catch((error) => {
+              if (error) res.send(error);
+            });
+        } else {
+          const preExistentCartItem = cart.items.find(
+            (item) => item.title == req.body.title
+          );
 
- addItem : (req, res) => {
-	Cart.findOne({"userID": req.body.userID})
-        .exec()
-        .then(cart => {
-			if (cart == null) {
-				cart = new Cart({
-					userID: req.body.userID,
-					items: [
-						{
-							"title": req.body.title,
-							"price": parseFloat(req.body.price),
-							"quantity": 1
-						}
-					],
-					totalCost: req.body.price
-				})
-				cart.save()
-					.then(result => {
-						res.send("Item added to cart");
-					})
-					.catch(error => {
-						if (error) res.send(error);
-					});
-			} else {
-				const preExistentCartItem = cart.items.find(item => item.title == req.body.title);
-				
-				if (preExistentCartItem) { 
-					preExistentCartItem.quantity = preExistentCartItem.quantity + 1;
-					cart.totalCost = cart.totalCost + parseFloat(req.body.price);
-					// solution for mongoose to keep track of changes in a mixed Array and being able to save those changes. 
-					// https://mongoosejs.com/docs/schematypes.html
-					// TODO: create a nested/ child schema for the array, so that mongoose can save without the markModified call.
-					cart.markModified('items')
-					cart.save()
-						.then(result => {
-							res.send("Item added to cart");
-						})
-						.catch(error => {
-							if (error) res.send(error);
-						});
-				} else {
-					cart.items.push(
-						{
-							"title": req.body.title,
-							"price": parseFloat(req.body.price),
-							"quantity": 1
-						}
-					);
-					cart.totalCost = cart.totalCost + parseFloat(req.body.price)
-					cart.save()
-						.then(result => {
-							res.send("Item added to cart");
-						})
-						.catch(error => {
-							if (error) res.send(error);
-						});
-				}
-			}
-        })
-        .catch((error) => {
-            console.log(error.message);
-            return [];
-        })
-},
+          if (preExistentCartItem) {
+            preExistentCartItem.quantity = preExistentCartItem.quantity + 1;
+            cart.totalCost = cart.totalCost + parseFloat(req.body.price);
+            // solution for mongoose to keep track of changes in a mixed Array and being able to save those changes.
+            // https://mongoosejs.com/docs/schematypes.html
+            // TODO: create a nested/ child schema for the array, so that mongoose can save without the markModified call.
+            cart.markModified("items");
+            cart
+              .save()
+              .then((result) => {
+                res.send("Item added to cart");
+              })
+              .catch((error) => {
+                if (error) res.send(error);
+              });
+          } else {
+            cart.items.push({
+              title: req.body.title,
+              price: parseFloat(req.body.price),
+              quantity: 1,
+            });
+            cart.totalCost = cart.totalCost + parseFloat(req.body.price);
+            cart
+              .save()
+              .then((result) => {
+                res.send("Item added to cart");
+              })
+              .catch((error) => {
+                if (error) res.send(error);
+              });
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        return [];
+      });
+  },
 
- removeItem : (req, res) => {
-	Cart.findOne({"userID": req.body.userID})
-	.exec()
-	
-	.then(cart => {
-		const preExistentCartItem = cart.items.find(item => item.title == req.body.title);
-		const newCartQuantity = preExistentCartItem.quantity - 1;
-		if (newCartQuantity != 0) {
-			preExistentCartItem.quantity = newCartQuantity;
-			cart.totalCost = cart.totalCost - parseFloat(req.body.price);
-			// solution for mongoose to keep track of changes in a mixed Array and being able to save those changes. 
-			// https://mongoosejs.com/docs/schematypes.html
-			// TODO: create a nested/ child schema for the array, so that mongoose can save without the markModified call.
-			cart.markModified('items')
-			cart.save()
-				.then(result => {
-					res.send("Item removed from cart");
-				})
-				.catch(error => {
-					if (error) res.send(error);
-				});
-		} else {
-			cart.items.pull(
-				{
-					"title": req.body.title,
-					"price": parseFloat(req.body.price),
-					"quantity": 1
-				}
-			);
-			cart.totalCost = cart.totalCost - parseFloat(req.body.price)
-			cart.save()
-				.then(result => {
-					res.send("Item removed from cart");
-				})
-				.catch(error => {
-					if (error) res.send(error);
-				});
-		}
-	})
-	.catch((error) => {
-		console.log(error.message);
-		return [];
-	})
-	
-},
+  removeItem: (req, res) => {
+    Cart.findOne({ userID: req.body.userID })
+      .exec()
 
-// TODO: inplement what to output when there was no shopping cart created yet. 
- get : (req, res) => {
-	// Cart.findOne({"userID": req.body.userID})
-	Cart.findOne({"userID": 1})
-        .exec()
-        .then(cart => {
-			if (cart == null) {
-			res.send("Empty shopping cart");
-			} else {
-				const cartDoc = cart;
-				const itemArray = cart.items;
-				var sizeCart = 0;
-				for (const item of itemArray){
-					sizeCart = sizeCart + item.quantity;
-				};
-				// TODO: decide whether to get rid of the totalPrice or not. 
-				// const totalPrice = cartDoc.totalCost;
-				var totalPrice = 0;
-				for (const item of itemArray){
-					totalPrice = totalPrice + item.quantity * item.price;
-				};
-				res.render("cart", {cart: cartDoc, cartSize: sizeCart, cartPrice: totalPrice});
-			}
-		})
-		.catch((error) => {
-            console.log(error.message);
-            return [];
-        })
-},
+      .then((cart) => {
+        const preExistentCartItem = cart.items.find(
+          (item) => item.title == req.body.title
+        );
+        const newCartQuantity = preExistentCartItem.quantity - 1;
+        if (newCartQuantity != 0) {
+          preExistentCartItem.quantity = newCartQuantity;
+          cart.totalCost = cart.totalCost - parseFloat(req.body.price);
+          // solution for mongoose to keep track of changes in a mixed Array and being able to save those changes.
+          // https://mongoosejs.com/docs/schematypes.html
+          // TODO: create a nested/ child schema for the array, so that mongoose can save without the markModified call.
+          cart.markModified("items");
+          cart
+            .save()
+            .then((result) => {
+              res.send("Item removed from cart");
+            })
+            .catch((error) => {
+              if (error) res.send(error);
+            });
+        } else {
+          cart.items.pull({
+            title: req.body.title,
+            price: parseFloat(req.body.price),
+            quantity: 1,
+          });
+          cart.totalCost = cart.totalCost - parseFloat(req.body.price);
+          cart
+            .save()
+            .then((result) => {
+              res.send("Item removed from cart");
+            })
+            .catch((error) => {
+              if (error) res.send(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        return [];
+      });
+  },
 
-// possible function for the nav bar? Still in progress
- countBasketItems : (req, res) => {
-	Cart.findOne({"userID": 1})
-        .exec()
-        .then(cart => {
-			const itemArray = cart.items;
-			var sizeCart = 0;
-			for (const item of itemArray){
-				sizeCart = sizeCart + item.quantity;
-			};
-			res.send(sizeCart.toString());
-		})
-		.catch((error) => {
-            console.log(error.message);
-            return [];
-        })
-},
+  // TODO: inplement what to output when there was no shopping cart created yet.
+  get: (req, res) => {
+    // Cart.findOne({"userID": req.body.userID})
+    Cart.findOne({ userID: 1 })
+      .exec()
+      .then((cart) => {
+        if (cart == null) {
+          res.send("Empty shopping cart");
+        } else {
+          const cartDoc = cart;
+          const itemArray = cart.items;
+          var sizeCart = 0;
+          for (const item of itemArray) {
+            sizeCart = sizeCart + item.quantity;
+          }
+          // TODO: decide whether to get rid of the totalPrice or not.
+          // const totalPrice = cartDoc.totalCost;
+          var totalPrice = 0;
+          for (const item of itemArray) {
+            totalPrice = totalPrice + item.quantity * item.price;
+          }
+          res.render("cart", {
+            cart: cartDoc,
+            cartSize: sizeCart,
+            cartPrice: totalPrice,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        return [];
+      });
+  },
 
+  // possible function for the nav bar? Still in progress
+  countBasketItems: (req, res) => {
+    Cart.findOne({ userID: 1 })
+      .exec()
+      .then((cart) => {
+        const itemArray = cart.items;
+        var sizeCart = 0;
+        for (const item of itemArray) {
+          sizeCart = sizeCart + item.quantity;
+        }
+        res.send(sizeCart.toString());
+      })
+      .catch((error) => {
+        console.log(error.message);
+        return [];
+      });
+  },
 
+  deliverOrder: (req, res) => {
+    Cart.updateMany({ userID: 1 }, { $set: { delivery: true, pickUp: false } })
+      .then((cart) => res.send(" the food will be delivered"))
+      .catch((err) => res.status(422).json(err));
+  },
 
-deliverOrder :  (req, res) => {
-	Cart.updateMany ({"userID": 1},{"$set":{"delivery":true}})
-  .then(cart => res.send(" the food will be delivered"))
-  .catch(err => res.status(422).json(err));
- },
-
- pickUpOrder :  (req, res) => {
-	Cart.updateMany ({"userID": 1},{"$set":{"pickUp":true}},
-	)
-  .then(res.send(" the food will be picked up"))
-  .catch(err => res.status(422).json(err));
- },
-
-//TO DO : MODIFY METHOD SO IT CHANGES THE BOOLEAN VALUE WITH IF STATMENTS
- // pickUpOrder :  (req, res) => {
-
-// 	Cart.findOneAndUpdate({"userID": 1} ,
-
-// 	{
-// 		$cond:  { if: {  $in: [ "cartSchema.$.pickUp", false ] }, then: true, else: true}
-// 	})
-
-//         .exec()
-//         .then(cart => {
-// 			cart.markModified('pickUp')
-// 			res.send(" the food will be picked up");
-		
-// 		})
-// 		.catch((error) => {
-//             console.log(error.message);
-//             return [];
-//         })
-
-
-// }
-
-
+  pickUpOrder: (req, res) => {
+    Cart.updateMany({ userID: 1 }, { $set: { pickUp: true, delivery: false } })
+      .then(res.send(" the food will be picked up"))
+      .catch((err) => res.status(422).json(err));
+  },
 };
