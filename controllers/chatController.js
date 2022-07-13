0cea7679806d16bbf495db2098122e286ee7f40e
Anni Nguyen <annie.ngyn@hotmail.com>
2022-07-13 15:20:10 +0200

    Completing lesson 31

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,13 +1,33 @@
+const message = require("../models/message");
+
+
 module.exports = io => {
     io.on("connection", client => {
         console.log("new connection");
+
         client.on("disconnect", () => {
             console.log("user disconnected");
         });
-        client.on("message", () => {
-            io.emit("message", {
-                content: "Hello"
-            });
+        
+        Message.find({})
+        .sort({ createdAt: -1 })
+        .limit(10)
+        .then(messages => {
+          client.emit("load all messages", messages.reverse());
         });
+
+        client.on("message", data => {
+            let messageAttributes = {
+              content: data.content,
+              userName: data.userName,
+              user: data.userId
+            };
+       m = new Message(messageAttributes);
+      m.save()
+        .then(() => {
+          io.emit("message", messageAttributes);
+        })
+        .catch(error => console.log(`error: ${error.message}`));
     });
+  });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -2,15 +2,44 @@ $(document).ready(() => {
 
     const socket = io();
 
-    $("#chatForm").submit((e) => {
-        socket.emit("message");
+    $("#chatForm").submit(() => {
+        let text = $("#chat-input").val(),
+        userName = $("#chat-user-name").val(),
+        userId = $("#chat-user-id").val();
+
+        socket.emit("message", {
+          content: text,
+          userName: userName,
+          userId: userId
+        });
+        
         $("#chat-input").val("");
         return false;
-    });
+      });
+
     socket.on("message", (message) => {
-        displayMessage(message.content);
+        displayMessage(message);
     });
-    let displayMessage = (message) => {
-        $("#chat").prepend($("<li>").html(message));
+
+    socket.on("load all messages", data => {
+      data.forEach(message => {
+        displayMessage(message);
+      });
+    });
+
+    let displayMessage = message => {
+      $("#chat").prepend(
+        $("<li>").html(`
+          <strong class="message ${getCurrentUserClass(message.user)}">
+            ${message.userName}
+          </strong>: ${message.content}
+        `)
+      );
     };
+
+   let getCurrentUserClass = (id) => {
+     let userId = $("#chat-user-id").val();
+     return userId === id ? "current-user": "";
+   };
 });
+
